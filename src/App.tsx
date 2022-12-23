6b0d87f720523697b27ec472ebac3bca10bb2e48
umasshia <giorgisamu.gs@gmail.com>
2022-12-22 23:15:43 -0500

    added tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
+import Tabs from "./components/Tabs";
 import Contact from "./pages/Contact";
 import Employment from "./pages/Employment";
 import Home from "./pages/Home";
@@ -11,13 +12,16 @@ function App() {
   return (
     <div className="main">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/employment.js" element={<Employment />}></Route>
-        <Route path="/projects.html" element={<Projects />}></Route>
-        <Route path="/contact.json" element={<Contact />}></Route>
-        <Route path="/resume.pdf" element={<Resume />}></Route>
-      </Routes>
+      <div className="page-wrap">
+        <Tabs />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/employment.js" element={<Employment />}></Route>
+          <Route path="/projects.html" element={<Projects />}></Route>
+          <Route path="/contact.json" element={<Contact />}></Route>
+          <Route path="/resume.pdf" element={<Resume />}></Route>
+          </Routes>
+      </div>
     </div>
   );
 }
