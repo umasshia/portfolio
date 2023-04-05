2dbb4434c1e082b43f6a03982524dd95bacd8400
umasshia <giorgisamu.gs@gmail.com>
2023-04-05 14:33:23 -0400

    updated message form again

--- src/components/Email/index.tsx
+++ src/components/Email/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import { send } from 'emailjs-com';
 import "./index.css";
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
@@ -11,42 +11,64 @@ const Email = () => {
     
     const [revealed, setRevealed] = useState(false)
     const [count, setCount] = useState(0)
-
+    const ref = useRef<HTMLTextAreaElement>(null);
+    const [sent, setSent] = useState(false)
 
     const [toSend, setToSend] = useState({
         from_email: "",
         subject: "",
-        message: ""
+        message: "",
+        send: ""
     });
+
+    const handleTextareaKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if(e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            handleSubmit(e)
+        }
+    }
+
+    const handleTextAreaSize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (ref.current) {
+            ref.current.style.height = "auto";
+            ref.current.style.height = `${e.target.scrollHeight - 16}px`;
+        }
+    };
 
     const handleSubmit = (e: FormEvent) => {
         console.log('event')
         const temp = toSend
-        if (temp.from_email === "" || temp.message === "" || temp.subject === "") {
+        if (temp.from_email === "" || temp.message === "" || temp.subject === "" || temp.send === "") {
             if (temp.from_email !== "") setCount(1)
             if (temp.subject !== "") setCount(2)
+            if (temp.message !== "") setCount(3)
         } else {
-            send(
-                SERVICE_ID,
-                TEMPLATE_ID,
-                toSend,
-                PUBLIC_KEY
-            )
-            .then((response) => {
-                console.log('SUCCESS!', response.status, response.text);
-                alert("Message Sent Successfuly :)")
-                setCount(0)
-                setToSend({
-                    from_email: "",
-                    subject: "",
-                    message: ""
-                }
+            if (temp.send.toLowerCase() === 'send') {
+                send(
+                    SERVICE_ID,
+                    TEMPLATE_ID,
+                    toSend,
+                    PUBLIC_KEY
                 )
-            })
-            .catch((err) => {
-                console.log('FAILED...', err);
-                alert("Something went wrong :(")
-            });
+                .then((response) => {
+                    console.log('SUCCESS!', response.status, response.text); 
+                    setTimeout(() => {
+                        setSent(true)
+                    }, 1000)
+                    setCount(0)
+                    setToSend({
+                        from_email: "",
+                        subject: "",
+                        message: "",
+                        send: ""
+                    }
+                    )
+                })
+                .catch((err) => {
+                    console.log('FAILED...', err);
+                    alert("Something went wrong :(")
+                });
+            }
         }
         e.preventDefault()
     };
@@ -56,7 +78,6 @@ const Email = () => {
     };
 
     return revealed ? (
-        
         <div className="email-container">
             <div className="header-bar">
                 <div className="write-me">
@@ -71,12 +92,12 @@ const Email = () => {
                 <div className="input">
                     <label htmlFor="from_email"><span className="o">o</span><span className="ugs">u@gs</span>:<span className="squiggle">~</span>$&nbsp;your email address:</label>
                     <input
-                        className="input field"
                         type="text"
                         name="from_email"
                         onChange={handleChange}
                         autoComplete="off"
                         autoFocus={count === 0}
+                        disabled={sent}
                     />
                 </div>
             </form>
@@ -85,12 +106,12 @@ const Email = () => {
                     <div className="input">
                         <label htmlFor="name"><span className="o">o</span><span className="ugs">u@gs</span>:<span className="squiggle">~</span>$&nbsp;subject:</label>
                         <input
-                            className="input field"
                             type="text"
                             name="subject"
                             onChange={handleChange}
                             autoComplete="off"
                             autoFocus={count > 0}
+                            disabled={sent}
                         />
                     </div>
                 </form>
@@ -98,20 +119,45 @@ const Email = () => {
             {count > 1 && (
                 <>
                     <form className="form" onSubmit={(e) => handleSubmit(e)}>
-                        <div className="input message">
-                            <label htmlFor="message"><span className="o">o</span><span className="ugs">u@gs</span>:<span className="squiggle">~</span>$&nbsp;your message:</label>
+                        <div className="input-message">
+                            <label htmlFor="message"><span className="o">o</span><span className="ugs">u@gs</span>:<span className="squiggle">~</span>$&nbsp;your message &#40;shift + enter for next line&#41;:</label>
                             <textarea
-                                className="input field"
+                                ref={ref}
                                 name="message"
-                                onChange={handleChange}
+                                onChange={(e) => { handleChange(e); handleTextAreaSize(e) }}
+                                onKeyDown={(e) => handleTextareaKeyDown(e)}
                                 autoComplete="off"
                                 autoFocus={count > 1}
+                                disabled={sent}
                             />
                         </div>
                     </form>
-                    <button className="submit" onClick={(e) => handleSubmit(e)} >Send Message</button>
                 </>
-                )}
+            )}
+            {count > 2 && (
+                <>
+                    <form className="form" onSubmit={(e) => handleSubmit(e)}>
+                        <div className="input">
+                            <label htmlFor="send"><span className="o">o</span><span className="ugs">u@gs</span>:<span className="squiggle">~</span>$&nbsp;type 'send' to send message:</label>
+                            <input
+                                type="text"
+                                name="send"
+                                onChange={handleChange}
+                                autoComplete="off"
+                                autoFocus={count === 3}
+                                disabled={sent}
+                            />
+                        </div>
+                    </form>
+                </>
+            )}
+            {sent && (
+                <div className="form">
+                    <div className="sent">
+                        <label htmlFor="sent"><span className="o">o</span><span className="ugs">u@gs</span>:<span className="squiggle">~</span>$&nbsp;message sent!</label>
+                    </div>
+                </div>
+            )}
         </div>
     ) : (
             <div className="header-bar">
