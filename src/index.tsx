5673885709c2764e5183dfefb94c31f368ac5795
umasshia <giorgisamu.gs@gmail.com>
2024-03-22 20:58:08 -0400

    basename added

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename='/portfolio'>
       <App />
     </HashRouter>
   </React.StrictMode>
